d4b39dcf483c3b4438c12a7cbddba9f9c2fa43f2
stb.infoserv <stb.infoserv@gmail.com>
2021-12-08 14:51:04 +0200

    MainWindow.xaml.cs refactored

--- Calculator MVVM/MainWindow.xaml.cs
+++ Calculator MVVM/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,54 +19,33 @@ namespace Calculator_MVVM
         public MainWindow()
         {
             InitializeComponent();
+            string test;
         }
 
-        private void matrix_rb_Checked(object sender, RoutedEventArgs e)
+        #region DictionaryConnections
+        private void Connect(string style)
         {
-            string style = "styles/Matrix.xaml";
+            style = "styles/" + style;
             Uri uri = new Uri(style, UriKind.Relative);
             ResourceDictionary res = (ResourceDictionary)Application.LoadComponent(uri);
             Application.Current.Resources.Clear();
             Application.Current.Resources.MergedDictionaries.Add(res);
         }
+        private void matrix_rb_Checked(object sender, RoutedEventArgs e) => Connect("Matrix.Xaml");
+        private void snowhite_rb_Checked(object sender, RoutedEventArgs e) => Connect("Snowhite.xaml");
+        private void bloody_rb_Checked(object sender, RoutedEventArgs e) => Connect("Bloody.xaml");
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
-        }
+        #endregion
 
-        private void btn_Close_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void snowhite_rb_Checked(object sender, RoutedEventArgs e)
-        {
-            string style = "styles/Snowhite.xaml";
-            Uri uri = new Uri(style, UriKind.Relative);
-            ResourceDictionary res = (ResourceDictionary)Application.LoadComponent(uri);
-            Application.Current.Resources.Clear();
-            Application.Current.Resources.MergedDictionaries.Add(res);
-        }
-
-        private void bloody_rb_Checked(object sender, RoutedEventArgs e)
-        {
-            string style = "styles/Bloody.xaml";
-            Uri uri = new Uri(style, UriKind.Relative);
-            ResourceDictionary res = (ResourceDictionary)Application.LoadComponent(uri);
-            Application.Current.Resources.Clear();
-            Application.Current.Resources.MergedDictionaries.Add(res);
-        }
-
+        #region WindowControl
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) => DragMove();
+        private void btn_Close_Click(object sender, RoutedEventArgs e) => Close();
         private void btn_restore_Click(object sender, RoutedEventArgs e)
         {
             if (this.WindowState == WindowState.Normal) this.WindowState = WindowState.Maximized;
             else this.WindowState = WindowState.Normal;
         }
-
-        private void btn_hide_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
+        private void btn_hide_Click(object sender, RoutedEventArgs e) => this.WindowState = WindowState.Minimized;
+        #endregion
     }
 }
